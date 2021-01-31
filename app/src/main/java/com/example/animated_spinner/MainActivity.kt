d268a2199f97d96b8f43eae74f3f35518a06baf7
Yousuf <aarajyousuf.qtec@gmail.com>
2021-01-31 13:16:21 +0600

    implement power spinner sdk

--- app/src/main/java/com/example/animated_spinner/MainActivity.kt
+++ app/src/main/java/com/example/animated_spinner/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.animated_spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.animated_spinner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        val questions = ArrayList<String>()
+        questions.add("What is your favourite color?")
+        questions.add("What is your favourite food?")
+        questions.add("What is your favourite pet`s name?")
+        questions.add("What is your favourite pet`s name?")
+        questions.add("What is your favourite coffee?")
+        questions.add("Why Kotlin?")
+        questions.add("What is your favourite color?")
+        questions.add("What is your favourite food?")
+        questions.add("What is your favourite pet`s name?")
+        questions.add("What is your favourite pet`s name?")
+        questions.add("What is your favourite coffee?")
+        questions.add("Why Kotlin?")
+        viewBinding.questionPSV.setItems(questions)
+        viewBinding.questionPSV.setOnSpinnerItemSelectedListener <String>{ _, _, index, item ->
+            Log.e("animated spinner:", item)
+            Toast.makeText(this, "selected item: $item position: $index", Toast.LENGTH_SHORT).show()
+        }
     }
 }
